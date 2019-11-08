bd86c01dda0c961e6aa50cae62a503f23b76d1f5
ThierryBissel <44964717+ThierryBissel@users.noreply.github.com>
2019-11-08 11:25:11 +0100

    Maj

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use((rep, res, next)=>{
-    res.status(200).json
-})
+    res.status(200).json({
+        massage: 'it works'
+    })
+});
+
+module.exports = app;
