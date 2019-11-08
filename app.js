b65d93bf54b4e44f1a11facf761ff2324694b5fc
ThierryBissel <44964717+ThierryBissel@users.noreply.github.com>
2019-11-08 16:18:44 +0100

    l'apres pause

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productsRoutes = require('./api/routes/product');
 const usersRoutes = require('./api/routes/users');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origins', '*');
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
+    if (req.method === 'OPTION'){
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+        res.status(200).json({});
+    }
+    next();
+})
+
 
     //Routes for normal requests
 app.use('/products', productsRoutes);
