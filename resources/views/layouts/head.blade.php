77ea280dc8b63d5fb3a5288482bdfabd482b7b0f
lhespel <44492009+lhespel@users.noreply.github.com>
2019-11-12 16:20:24 +0100

    maj front

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,4 +5,14 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #101010;
+            color: white;
+            text-align: center;
+        }
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
 </html>
    <body>
     @extends('layouts.nav')
+   
     </body>
 
 
