ee20f03b45b2a8bfed13929a2e9f50b03d4fa7e3
ThierryBissel <44964717+ThierryBissel@users.noreply.github.com>
2019-11-13 23:50:24 +0100

    on peut enfin crer un compte

--- api/models/user.js
+++ api/models/user.js
@@ -9,7 +9,33 @@ var User = function(user){
     this.role = user.role;
 };
 
+User.findAUser = function (usermail, result) {
+    sql.query("SELECT mail FROM user WHERE mail = ? ",usermail, function(err, res){
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else{
+            console.log(res);
+            result(null, res);
+        }
+    })
+}
 
+    //Create a user
+User.createUser = function (newuser, result) {
+    usrsql.query("INSERT INTO user set ?", newuser, function(err, res){
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else{
+           console.log(res.insertId);
+            result(null, res.insertId);
+        }
+    })
+        
+}
 
 
 module.exports = User;
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,31 +5,28 @@ const bcrypt = require('bcrypt');
  
 
 router.get('/register', (req, res, next) => {
-    bcrypt.hash(req.body.email, 10, (err, hash) =>{
-        if(err){
-            return res.status(500).json({
-                error: err                    
+    User.findAUser(req.body.mail, function(err, user) {
+        if (err) {
+            res.send(err);
+        }
+        if (res(mail) = req.body.mail){
+            return res.status(409).json({
+                message: "mail exists"
             });
         }
         else{
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(result=>{
-                    res.status(201).json({
-                        message: 'user created'
+            bcrypt.hash(req.body.password, 10, (err, hash) =>{
+                if(err){
+                    return res.status(500).json({
+                        error: err                    
                     });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
-                    })
-                });
+                }
+                else{
+                    var user = new User(req.body);
+                }
+            })
         }
-    }) 
+    }); 
 })
 
 router.get('/login', (req, res, next) => {
