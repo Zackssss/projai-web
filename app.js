61b9f1400f2c0deef5c2584ffa68cf75d20a747b
ThierryBissel <44964717+ThierryBissel@users.noreply.github.com>
2019-11-13 15:57:50 +0100

    yay la seul vrai route

--- app.js
+++ app.js
@@ -3,5 +3,38 @@ const app = express();
 
 const usersRoutes = require('./api/routes/users');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origins', '*');
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
+    if (req.method === 'OPTION'){
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+        res.status(200).json({});
+    }
+    next();
+})
+
+app.use('/users', usersRoutes);
+
+    //Error routes
+    app.use((req, res, next) =>{
+        const error = new Error('Not found');
+        error.status = 404; 
+        next(error);   
+    })
+    
+        //Error handeling
+    app.use((error, req, res, next)=>{
+        res.status(error.status||500);
+        res.json({
+            error:{
+                message: error.message
+            }
+        })
+    })
+    
 
 module.exports = app;
