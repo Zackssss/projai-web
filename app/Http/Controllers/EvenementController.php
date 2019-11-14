7e6130ef53066a901b2ec1ac4275a02090d092bd
Zackssss <zack29morello12@gmail.com>
2019-11-14 13:10:32 +0100

    ca va le faire

--- app/Http/Controllers/EvenementController.php
+++ app/Http/Controllers/EvenementController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Carbon\Carbon;
 use App\Evenement as Evenement;
 
 
@@ -13,12 +13,29 @@ class EvenementController extends Controller
 
         return view('createEvent');
     }
-    public function EventCeMois()
+    public function EventMois()
     {
-        $url = url()->full() ;
+        $dt = Carbon::now(); // setting date
+        $dt-> subMonth();
+        $dt=$dt->toDateString();
 
-        $id=substr($url, -1);
+        $Evenement = Evenement::where('date_evenement','>',$dt)->get(); //getting all event according to date
 
+        return view('event_mois')-> with('Evenement', $Evenement);
+
+    }
+    public function eventpassé(){
+
+        $dt = Carbon::now(); // setting date
+        $dt-> subMonth();
+        $dt=$dt->toDateString();
+
+        $Evenement = Evenement::where('date_evenement','<',$dt)->get(); //getting all event according to date
+
+        return view('event_mois')-> with('Evenement', $Evenement);
+    }
+    public function eventcemois($id)
+    {
 
 
         $Evenement = Evenement::where('evenements.id_evenement',$id)->leftjoin('images','evenements.id_evenement','=','images.id_evenement')
@@ -27,14 +44,16 @@ class EvenementController extends Controller
 
 
         return view('event')-> with('Evenement', $Evenement);
-
     }
-    public function eventpassé(){
+   
+    public function index()
+    {
+        $evenement = Evenement::paginate(100);
 
-        $event = Evenement::all();
-        return view('event_mois')-> with('Evenement', $event);
+        return Evenement::collection($evenement);
     }
-   public function store(){
+
+        public function store(){
 
         $evenement = new Evenement();
         $evenement->nom_evenement = request('nom_evenement');
@@ -46,27 +65,6 @@ class EvenementController extends Controller
         $evenement->save();
         return "Evenement sauvegardé !";
     }
-    public function index()
-    {
-        $evenement = Evenement::paginate(100);
-
-        return Evenement::collection($evenement);
-    }
-    /*ublic function store(Request $request)
-    {
-        $evenement = $request->isMethod('put') ? Evenement::findOrFail($request->id) : new Evenement;
-        $evenement->id = $request->input('id');
-        $evenement->nom_evenement = $request->input('nom_evenement');
-        $evenement->association = $request->input('association');
-        $evenement->description_evenement = $request->input('description_evenement');
-        $evenement->date_evenement = $request->input('date_evenement');
-        $evenement->recurrence = $request->input('recurrence');
-        $evenement->prix = $request->input('prix');
-
-        if ($evenement->save()) {
-            return new Evenement($evenement);
-        }
-    }*/
     public function show($id)
     {
         $evenement = Evenement::findOrFail($id);
