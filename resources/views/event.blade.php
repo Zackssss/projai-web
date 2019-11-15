0c09b78bafeeabad52ce2fbac7d3d69a6d39d0d9
valentin <valentin.reinbold@viacesi.fr>
2019-11-15 09:39:42 +0100

    remove button in event

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -52,7 +52,7 @@ float: left;
     <div class="navbar-header"> 
             <a class="navbar-brand" href="/dljsonevent/{{$Evenement-> id_evenement}}">Download Json File</a>
     </div>
-    <a class="navbar-brand" >Ajouter un event</a>
+    
     @endsection
     @if ($index==1)
     <div class="Event">
